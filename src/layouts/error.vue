6f9a4bc732b7bec7cdaf2f8a803e6dedac7e3161
huangkuiwei <1017969390@qq.com>
2021-01-13 23:17:53 +0800

    add error.vue

---
+++ src/layouts/error.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="error">
+    <div>页面发生错误</div>
+    <div>{{ JSON.stringify(error) }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'error',
+  props: {
+    error: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+}
+</script>
+
+<style scoped></style>
